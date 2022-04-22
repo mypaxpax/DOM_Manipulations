7ba5deb0291f2ae4bbd3ac75d871618b362a7b3e
mypaxpax <thomasgade@gmail.com>
2022-04-22 14:42:46 +0200

    Re-wrote the script and added documentation.

---
+++ script.js
@@ -0,0 +1,42 @@
+const container = document.querySelector("#container");
+const content = document.createElement("container");
+content.classList.add("content");
+content.textContent ="this is glorius text-content";
+container.appendChild(content);
+
+// Create the <p> element and append it to the content DIV
+let para = document.createElement("p");
+para.style.color = "red";
+para.textContent = "Hey, I'm red!";
+content.appendChild(para);
+
+// Create the <3> element and append it to the content DIV
+let h3 = document.createElement("h3")
+h3.style.color = "blue";
+h3.textContent = "I'm blue h3";
+content.appendChild(h3);
+
+// Create the newDiv element and store it's variable as "newDiv"
+const newDiv = document.createElement("div");
+newDiv.style.backgroundColor = "pink";
+newDiv.style.border = "solid black 1px";
+
+// Create the <h1> element and append it to the newDiv
+let h1 = document.createElement("h1");
+h1.textContent = "I'm in a div";
+newDiv.appendChild(h1);
+
+// Create the second <p> element and append it to the newDiv.
+let newPara = document.createElement("p");
+newPara.textContent = "Me too!";
+newDiv.append(newPara);
+
+// finally we append the newDiv to the container
+container.appendChild(newDiv);
+
+
+// const content = document.createElement(c'div');
+// content.classList.add('content');
+// content.textContent = 'This is the glorious text-content!';
+
+// container.appendChild(content);
